de54584720d005c663c91f4bc7d3b6d4b637beea
Nathaniel Mention <nathanielmention@gmail.com>
2020-08-12 22:38:52 -0400

    add App component

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,5 @@
 import React from "react";
 import { render } from "react-dom";
+import App from "./components/app";
 
-function Hi() {
-  return <p>Hi.</p>;
-}
-
-render(<Hi />, document.getElementById("app"));
+render(<App />, document.getElementById("root"));
