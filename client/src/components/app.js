aa5f37b0342ed09f916c23fc1fe15c49c4876cea
Nathaniel Mention <nathanielmention@gmail.com>
2020-08-15 18:00:32 -0400

    import react router dom, add home/auth components

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+//pages for app
+import Home from "./home/Home";
+import Login from "./login/Login";
+import Register from "./register/Register";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { render } from "react-dom";
+import ReactDOM from "react-dom";
 import App from "./components/app";
+import { BrowserRouter } from "react-router-dom";
 
-render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
